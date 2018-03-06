5577cc3af894988a35913969498b8f0b1fa85f52
Raimo Johanson <raimo.johanson@gmail.com>
2018-03-06 17:57:44 +0000

    update

---
+++ knexfile_example.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: "localhost",
+      //port: "5432",
+      user: "",
+      //password: "demo",
+      database: "",
+      charset: "UTF8",
+      timezone: "EET",
+    },
+    debug: false
+  }
+
+};
